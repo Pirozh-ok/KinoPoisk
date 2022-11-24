0845e76affce63c29d620ae8c06ae7c48231ab3f
Иван Воротников <ivan-vorotnikov@outlook.com>
2022-11-24 19:03:31 +0700

    Added presentation layer

---
+++ KinoPoisk.PresentationLayer/Extensions/AddDbContextExtensions.cs
@@ -0,0 +1,16 @@
+﻿using KinoPoisk.DataAccess;
+using KinoPoisk.DomainLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace KinoPoisk.PresentationLayer.Extensions {
+    public static class AddDbContextExtensions {
+        public static void AddDbConnection(this IServiceCollection services, string connectionString) {
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseSqlServer(connectionString);
+            })
+                .AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>(); 
+        }
+    }
+}
---
+++ KinoPoisk.PresentationLayer/Program.cs
@@ -0,0 +1,27 @@
+using KinoPoisk.PresentationLayer.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbConnection(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
